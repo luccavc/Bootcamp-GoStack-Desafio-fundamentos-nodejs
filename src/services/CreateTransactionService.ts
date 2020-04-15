7680ed8252b7fc5a680205676fc259fe6b04357c
Lucca Cacella <lucca.cacella@gmail.com>
2020-04-15 12:01:07 -0300

    exercício finalizado

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface TransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,23 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: TransactionDTO): Transaction {
+    if (
+      type === 'outcome' &&
+      this.transactionsRepository.getBalance().total - value < 0
+    ) {
+      throw Error(
+        'Outcome transaction value exceeds total value of your account',
+      );
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transaction;
   }
 }
 
